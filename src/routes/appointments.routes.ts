d31d95444602aeb44d2086fc110f3d07440fbd87
daniel <daniel_fmpc@hotmail.com.br>
2020-07-08 10:48:00 -0300

    tirando inteface e adicionando o model

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,16 +1,10 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointments';
 
 const appointmentsRoute = Router();
 
-interface AppointmentsDTO {
-  id: string;
-  provider: string;
-  date: Date;
-}
-
-const appointments: AppointmentsDTO[] = [];
+const appointments: Appointment[] = [];
 
 appointmentsRoute.get('/', (request, response) => {
   return response.json(appointments);
@@ -31,11 +25,7 @@ appointmentsRoute.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
