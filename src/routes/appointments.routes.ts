8e4a370e5973d001b7401159fa27fe2693586b39
daniel <daniel_fmpc@hotmail.com.br>
2020-07-08 10:22:43 -0300

    adicionando tipagem e pacote uuid

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,13 +1,28 @@
 import { Router } from 'express';
-
+import { uuid } from "uuidv4";
 const appointmentsRoute = Router();
 
+interface DTO{
+  provider: string,
+  date: Date
+};
+
+const appointments:DTO[] = [];
 appointmentsRoute.get('/', (request, response) => {
-  return response.json({ message: 'ok' });
+  return response.json(appointments);
 });
 
-appointmentsRoute.post('/appointments/:id', (request, response) => {
-  return response.json({ message: 'ok' });
+appointmentsRoute.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const appointment = {
+    provider,
+    date,
+  }
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRoute;
