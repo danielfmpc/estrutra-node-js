1c9369cfa5b6c696f286e761ed8e13b363f1da0e
daniel <daniel_fmpc@hotmail.com.br>
2020-07-09 16:01:35 -0300

    exibindo imagem

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import express from 'express';
 import routes from './routes';
 
 import './database';
+import upload from './config/upload';
 
 const app = express();
 
 app.use(express.json());
+app.use('/file', express.static(upload.directory));
 app.use(routes);
 
 app.listen(3333, () => {
