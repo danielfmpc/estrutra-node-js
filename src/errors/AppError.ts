27fba653240d07facce25ef454d1f723f160b873
daniel <daniel_fmpc@hotmail.com.br>
2020-07-09 16:04:21 -0300

    criando handleError

---
+++ src/errors/AppError.ts
@@ -0,0 +1,12 @@
+class Error {
+  public readonly message: string;
+
+  public readonly statuscode: number;
+
+  constructor(message: string, statusCode = 400) {
+    this.message = message;
+    this.statuscode = statusCode;
+  }
+}
+
+export default Error;
